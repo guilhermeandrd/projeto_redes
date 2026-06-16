bacb0a360acf719ea9fd31eb5ab8830bc646288c
guilherme.andrd <nickyandrade331@gmail.com>
2026-06-16 18:54:10 -0300

    fix: encerramento do cliente

--- client.py
+++ client.py
@@ -6,7 +6,7 @@
 
 from socket import *
 
-serverName = '127.0.0.1'
+serverName = '192.168.1.1'
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -15,9 +15,11 @@ clientSocket.connect((serverName, serverPort))
 while True:
     sentence = input('Input sentence: ')
     clientSocket.send(sentence.encode())
-    modifiedSentence = clientSocket.recv(1024)
-    if(modifiedSentence.decode().strip() == 'X'):
+
+    if sentence == 'X':
         break;
+    
+    modifiedSentence = clientSocket.recv(1024)
     print('From Server:', modifiedSentence.decode())
 
 print('Conexão encerrada')
